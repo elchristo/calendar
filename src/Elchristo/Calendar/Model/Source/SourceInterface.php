db726e47cc1fae28c45ff553e6bcc9acca7a7a9b
Christoph Lehner <cpl@elchristo.de>
2017-05-19 21:57:55 +0200

    Add default methods to source interface

--- src/Elchristo/Calendar/Model/Source/SourceInterface.php
+++ src/Elchristo/Calendar/Model/Source/SourceInterface.php
@@ -7,7 +7,11 @@ namespace Elchristo\Calendar\Model\Source;
  */
 interface SourceInterface
 {
+    public function getIdentifier();
     public function getEvents();
     public function getEventBuilder();
     public function setEventBuilder($builder);
+    public function getFetchedResults();
+    public function getOptions();
+    public function getCriteria();
 }
